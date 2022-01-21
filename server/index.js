a6e585f1c60d7d8140ab186ef9d0c3fe8d1481ff
indypeters2006 <46998095+indypeters2006@users.noreply.github.com>
2022-01-21 10:04:41 +0100

    Revert "wip"
    This reverts commit 7e0c6390fd7c1ddc7c4eb811fbead25d92434bd2.

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express()
 const port = 3000
 
-app.get('/login', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/index.html'));
 })
 
